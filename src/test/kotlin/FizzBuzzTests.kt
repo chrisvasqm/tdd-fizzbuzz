ca578880c8f06264cc90c350005eae52510d6a38
chrisvasqm <christianvasquezmelo@gmail.com>
2022-03-03 20:43:13 -0400

    Add first test

---
+++ src/test/kotlin/FizzBuzzTests.kt
@@ -0,0 +1,10 @@
+import org.testng.annotations.Test
+
+class FizzBuzzTests {
+
+    @Test
+    fun fizzbuzz_IsMultipleOfThree_ReturnsFizz() {
+        TODO("Waiting to be implemented...")
+    }
+
+}
