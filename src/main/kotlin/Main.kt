c175d7b4c2bc4ddd24fdf3e224758ac1b19aec4a
chrisvasqm <christianvasquezmelo@gmail.com>
2022-03-15 06:36:55 -0400

    Solve exercise

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,24 @@
+// Count the numbers from 1 to 15
+// If the number is divisible by 3, print Fizz [X]
+// If the number is divisible by 5, print Buzz [X]
+// If the number is divisible by both 3 and 5, print FizzBuzz [X]
 fun main(args: Array<String>) {
-    println("Hello World")
+    for (number in 1..15) {
+        println(fizzBuzz(number))
+    }
 }
+
+fun fizzBuzz(number: Int): String {
+    if (isDivisibleBy(15, number))
+        return "FizzBuzz"
+
+    if (isDivisibleBy(3, number))
+        return "Fizz"
+
+    if (isDivisibleBy(5, number))
+        return "Buzz"
+
+    return number.toString()
+}
+
+private fun isDivisibleBy(by: Int, number: Int) = number % by == 0
--- src/test/kotlin/FizzBuzzTests.kt
+++ src/test/kotlin/FizzBuzzTests.kt
@@ -3,8 +3,17 @@ import org.testng.annotations.Test
 class FizzBuzzTests {
 
     @Test
-    fun fizzbuzz_IsMultipleOfThree_ReturnsFizz() {
-        TODO("Waiting to be implemented...")
+    fun `Is divisible of 3, then return Fizz`() {
+        assert(fizzBuzz(3) == "Fizz")
     }
 
+    @Test
+    fun `Is divisible of 5, then return Buzz`() {
+        assert(fizzBuzz(5) == "Buzz")
+    }
+
+    @Test
+    fun `Is divisible of 15, then return FizzBuzz`() {
+        assert(fizzBuzz(15) == "FizzBuzz")
+    }
 }
