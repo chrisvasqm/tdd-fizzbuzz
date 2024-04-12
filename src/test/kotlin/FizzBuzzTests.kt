b9437c4f5b9fc289f86970d2fd75cae48d476335
chrisvasqm <christianvasquezmelo@gmail.com>
2024-04-12 18:28:26 -0400

    Add test

--- src/test/kotlin/FizzBuzzTests.kt
+++ src/test/kotlin/FizzBuzzTests.kt
@@ -1,19 +1,25 @@
-import org.testng.annotations.Test
+import org.junit.jupiter.api.Test
 
 class FizzBuzzTests {
 
     @Test
-    fun `Is divisible of 3, then return Fizz`() {
+    fun `Is divisible of 3, then returns Fizz`() {
         assert(fizzBuzz(3) == "Fizz")
     }
 
     @Test
-    fun `Is divisible of 5, then return Buzz`() {
+    fun `Is divisible of 5, then returns Buzz`() {
         assert(fizzBuzz(5) == "Buzz")
     }
 
     @Test
-    fun `Is divisible of 15, then return FizzBuzz`() {
+    fun `Is divisible of 15, then returns FizzBuzz`() {
         assert(fizzBuzz(15) == "FizzBuzz")
     }
+
+    @Test
+    fun `Is not divisible by neither 3, 5 or both, then returns same number`() {
+        assert(fizzBuzz(1) == "1")
+    }
+
 }
